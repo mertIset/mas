fa6c49d2ef3725c883ab3f59aa904f91f9d481e3
mertiset <mertiset11@gmail.com>
2025-06-26 15:08:28 +0200

    Initialisierung

---
+++ external-task-worker/src/main/java/de/htwberlin/mas/external_task_worker/ExternalTaskWorkerApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.mas.external_task_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalTaskWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalTaskWorkerApplication.class, args);
+	}
+
+}
---
+++ external-task-worker/src/test/java/de/htwberlin/mas/external_task_worker/ExternalTaskWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.mas.external_task_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalTaskWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
